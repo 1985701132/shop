5f3dcd7d4dfe31ef8eff22fb15da49f35d39b93a
1985701132@qq.com <1985701132@qq.com>
2018-09-25 17:10:43 +0800

    初始化项目

---
+++ controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+namespace controllers;
+class IndexController
+{
+    function index()
+    {
+        echo '111111111111';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+define('ROOT',__DIR__.'/../');
+
+/**
+ * 类的自动加载
+ */
+
+function load($class)
+{
+    $path = str_replace('\\','/',$class);
+    require ROOT.$path.'.php';
+}
+spl_autoload_register('load');
+
+/**
+ * 解析路由
+ */
+
+$controller = "\controllers\\IndexController";
+$action = "index";
+
+if(isset($_SERVER['PATH_INFO']))
+{
+    $route = explode("/",$_SERVER['PATH_INFO']);
+    $controller = "\controllers\\". ucfirst($route[1])."Controller";
+    $action = @$route[2] ? @$route[2] : "index"; 
+}
+
+$c = new $controller;
+$c->$action();
